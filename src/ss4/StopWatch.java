0945a989ff854696266dd7e560be6d6a2a79af71
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-03-10 05:47:53 +0700

    nop bai ss4

--- src/ss4/StopWatch.java
+++ src/ss4/StopWatch.java
@@ -1,16 +1,50 @@
 package ss4;
 
-import java.time.LocalTime;
+import java.util.Scanner;
+
+public class StopWatch {
+    long starttime;
+    long endTime;
 
 
-    public  class StopWatch {
-        private LocalTime startTime, endTime;
-        public StopWatch(LocalTime startTime, LocalTime endTime) {
-            this.startTime = startTime;
-            this.endTime = endTime;
-        }
-        public int getElapsedTime(){
-            int milliseconds = ((endTime.getHour()-startTime.getHour())*3600 + (endTime.getMinute()-startTime.getMinute())*60 + (endTime.getSecond()-startTime.getSecond())*1000);
-            return milliseconds;
+    public StopWatch(long starttime, long endTime) {
+        this.starttime = starttime;
+        this.endTime = endTime;
+    }
+
+    public long getStarttime() {
+        return starttime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public static long start() {
+        long a1 = System.currentTimeMillis();
+        return a1;
+    }
+
+    public static long stop() {
+        long b1 = System.currentTimeMillis();
+        return b1;
+    }
+
+    public static long getElapsedTime(long a, long b) {
+        long c = b - a;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        long a = start();
+        System.out.println(a);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập vào 1 để ngừng thời gian ");
+        int click = input.nextInt();
+        if (click == 1) {
+            long b = stop();
+            System.out.println(b);
+            System.out.println("Thời gian  đã trôi qua là :" + getElapsedTime(a, b) + " milisSecond");
         }
     }
+}
